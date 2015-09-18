dc61ff7b7ac0f558f4ad143b9d483e0ffd08d5bb
rafaellynn <rafa.fernandes404@gmail.com>
2015-09-18 02:40:15 -0300

    Update extension.js

--- extension.js
+++ extension.js
@@ -46,8 +46,8 @@
     //Change the bots default settings and make sure they are loaded on launch
 
     localStorage.setItem("basicBotsettings", JSON.stringify({
-      botName: "basicBot",
-      language: "english",
+      botName: "Mix Bot",
+      language: "portuguese",
       chatLink: "https://rawgit.com/bscBot/source/master/lang/en.json",
       scriptLink: "https://rawgit.com/bscBot/source/master/basicBot.js",
       roomLock: false, // Requires an extension to re-load the script
